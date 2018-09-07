74211efbe4856c81fb2f6e8fca447d604e9c6008
GensokyoLRE <31522695+GensokyoLRE@users.noreply.github.com>
2018-09-06 18:12:48 -0700

    CorrectedVersion
    And with this I'm out of the running for Top Gun.

--- OrbOfConfusion.py
+++ OrbOfConfusion.py
@@ -2,6 +2,8 @@ from math import pi
 try:
     radius = float(input("Enter the radius of the circle: "))
     volume = (4 / 3) * pi * (radius ** 3)
-    print("It's actually a... SPHERE. With a volume of " + str(round(volume, 2)))
+    print("It's actually a... SPHERE. With a diameter of " + str(radius*2) + ", circumference of " +
+          str(round(2*pi*radius, 2)) + ", surface area of " + str(round(4*pi*(radius**2), 2)) + ", and volume of "
+          + str(round(volume, 2)))
 except ValueError:
     print("IT'S AN IMPOSSIBLE OBJECT. THAT'S AN IMPOSSIBLE OBJECT. (a.k.a. doesn't work here)")
